d368210a5d17004fc8df33b6a9b64df49d8a4f6e
zhanghaoran <zhanghaoran@xiaomi.com>
2018-08-17 17:10:34 +0800

    rm debug

--- cmake-build-debug/cxxreact/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harry/priProjects/personal/react-native/ReactCommon/cxxreact/tests
-# Build directory: /home/harry/priProjects/personal/react-native/ReactCommon/cmake-build-debug/cxxreact/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(jsctest "jsctest")
--- cmake-build-debug/microprofiler/CMakeFiles/microprofiler.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmicroprofiler.a"
-)
